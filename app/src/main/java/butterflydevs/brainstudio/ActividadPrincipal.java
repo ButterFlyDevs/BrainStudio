e464d1008454f6dff53bff3440b81cf4a81685bf
juanAFernandez <juan.antonio.fernandez.sanchez.88@gmail.com>
2015-05-29 20:40:04 +0200

    Modificada actividad principal

--- app/src/main/java/butterflydevs/brainstudio/ActividadPrincipal.java
+++ app/src/main/java/butterflydevs/brainstudio/ActividadPrincipal.java
@@ -1,39 +1,138 @@
 package butterflydevs.brainstudio;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
+public class ActividadPrincipal extends Activity {
 
-public class ActividadPrincipal extends ActionBarActivity {
+    private CircularCounter meter, meter2;
+
+    private String[] colors;
+
+    private Handler handler;
+
+    private Runnable r;
+
+    private Button botonBrain;
+
+    private TextView customFont, customFont2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_principal);
-    }
+        Animation loopParpadeante = AnimationUtils.loadAnimation(this, R.anim.animacionbotonplay);
 
+        colors = getResources().getStringArray(R.array.colors);
+        botonBrain = (Button)findViewById(R.id.button);
+
+        botonBrain.startAnimation(loopParpadeante);
+
+
+        botonBrain.setOnClickListener(
+
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Creamos el Intent
+                       // Intent intent = new Intent(ActividadPrincipal.this, ActividadNiveles.class);
+                        //Iniciamos la nueva actividad
+                        //startActivity(intent);
+                    }
+                }
+        );
+
+
+        meter = (CircularCounter) findViewById(R.id.meter);
+        // meter2 = (CircularCounter) findViewById(R.id.meter2);
+
+        meter.setFirstWidth(getResources().getDimension(R.dimen.first))
+                .setFirstColor(Color.parseColor(colors[0]))
+
+                        //.setSecondWidth(getResources().getDimension(R.dimen.second))
+                        //.setSecondColor(Color.parseColor(colors[1]))
+
+                        //.setThirdWidth(getResources().getDimension(R.dimen.third))
+                        //.setThirdColor(Color.parseColor(colors[2]))
+                .setBackgroundColor(Color.TRANSPARENT);
+        //.setBackgroundColor(-14606047);
+
+        meter.setMetricText("");
+        //meter.setMetricSize(30.f);
+        meter.setRange(100);
+        meter.setTextColor(Color.GRAY);
+        meter.setTextSize(40.f);
+
+        customFont = (TextView)findViewById(R.id.textView);
+        customFont2 = (TextView)findViewById(R.id.textView2);
+        Typeface font = Typeface.createFromAsset(getAssets(), "gloriahallelujah.ttf");
+        customFont.setTypeface(font);
+        customFont2.setTypeface(font);
+        meter.setTypeface(font);
+
+
+
+
+        handler = new Handler();
+        r = new Runnable(){
+
+            int currV = 0;
+            boolean go = true;
+
+            public void run(){
+
+                /*
+                if(currV == 60 && go)
+                    go = false;
+                else if(currV == -60 && !go)
+                    go = true;
+
+                if(go)
+                    currV++;
+                else
+                    currV--;
+                */
+                if(currV<46)
+                    currV++;
+
+
+
+                meter.setValues(currV, 0, 0);
+                //  meter2.setValues(currV, currV*2, currV*3);
+
+                handler.postDelayed(this, 30);
+            }
+        };
+
+
+
+
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_actividad_principal, menu);
-        return true;
+    protected void onResume(){
+        super.onResume();
+        handler.postDelayed(r, 500);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onPause(){
+        super.onPause();
+        handler.removeCallbacks(r);
     }
+
+
 }
