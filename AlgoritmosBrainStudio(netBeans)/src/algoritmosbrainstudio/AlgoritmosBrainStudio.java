f393535669a9eafec3879d74d54cb3acad9a5207
juanAFernandez <juan.antonio.fernandez.sanchez.88@gmail.com>
2015-06-01 00:07:42 +0200

    Terminado primer algoritmo y pasado a AndroidStudio

--- AlgoritmosBrainStudio(netBeans)/src/algoritmosbrainstudio/AlgoritmosBrainStudio.java
+++ AlgoritmosBrainStudio(netBeans)/src/algoritmosbrainstudio/AlgoritmosBrainStudio.java
@@ -15,11 +15,21 @@ import java.util.Random;
  */
 public class AlgoritmosBrainStudio {
 
+    //Por si queremos obtener depuración de los algoritmos por terminal.
+    static boolean depuracion =false;
     
-    static public boolean[] obtenerMatrizJugada(int numCeldas, int tamGrid){
+    /**
+     * Esta es la función que se copiará en el código de AndroidStudio
+     * @param numCeldas El número de celdas a con las que jugar.
+     * @param numFilas El número de filas que tiene el grid de la jugada.
+     * @param numColumnas El número de columnas que tiene el grid de la jugada.
+     * @return 
+     */
+    static public boolean[] obtenerMatrizJugada(int numCeldas, int numFilas, int numColumnas){
 
+        int tamGrid=numFilas*numColumnas;
         //Creamos el vector que vamos a devolver:
-        boolean [] matrizBooleanos = new boolean[12];
+        boolean [] matrizBooleanos = new boolean[tamGrid];
 
         //Inicializamos el vector:
         for(int i=0; i<tamGrid; i++)
@@ -33,9 +43,11 @@ public class AlgoritmosBrainStudio {
                 for(int i=0; i<tamGrid; i++)
                     enteros.add(i);
                 
+                if(depuracion){
                 for(int elemento: enteros)
                     System.out.print(elemento);
                 System.out.println("");
+                }
                     
 
         ArrayList finales = new ArrayList();
@@ -47,8 +59,9 @@ public class AlgoritmosBrainStudio {
             Random rnd = new Random();
             for(int i=0; i<numCeldas; i++) {
                 elegido = (int)(rnd.nextDouble() * numFinal); //Random de números entre el 0 y el numFinal
-                System.out.println("elegido: "+elegido);
+                if(depuracion) System.out.println("elegido: "+elegido);
                 
+                if(depuracion){
                 System.out.println("Vector de enteros antes");
                 for(int elemento: enteros)
                     System.out.print(elemento);
@@ -59,7 +72,7 @@ public class AlgoritmosBrainStudio {
                 for(int elemento: enteros)
                     System.out.print(elemento);
                 System.out.println("");
-                
+                }
                 
                 finales.add(enteros.get(elegido));
                 enteros.remove(elegido);
@@ -77,13 +90,19 @@ public class AlgoritmosBrainStudio {
         return matrizBooleanos;
     }
     
+    /**
+     * Para mostrar la matriz en el terminal
+     * @param matriz Matriz de booleanos a leer
+     * @param fil //Número de filas de la misma
+     * @param col  //Número de columnas de la misma
+     */
     static public void leerMatriz(boolean matriz[], int fil, int col){
         int columnas=1;
         for(int i=0; i<fil*col; i++){
             if(matriz[i]==true)
-                System.out.print(1);
+                System.out.print(1+" ");
             else
-                System.out.print(0);
+                System.out.print(0+" ");
             if(columnas==col){
                 System.out.print("\n");
                 columnas=1;
@@ -97,13 +116,63 @@ public class AlgoritmosBrainStudio {
     }
     
     /**
+     * Función para comparar la matriz original y la que crea el usuario
+     * @param matrizOriginal La matriz de referencia (la correcta)
+     * @param matrizJugador  La matriz que crea el jugador al pulsar los botones
+     * @param numFilas Número de filas de las matrices 
+     * @param numColumnas Número de columnas de las matrices
+     * @return true si las matrices son iguales y false si no lo son
+     */
+    static public boolean compruebaMatrices(boolean matrizOriginal[], boolean matrizJugador[], int numFilas, int numColumnas){
+        
+        /*
+        El único objetivo es compara si dos matrices son iguales. Para eso lo vamos a realizar celda a celda.
+        */
+        
+        boolean resultado=true;
+        int tamGrid=numFilas*numColumnas;
+      
+        /*Forma más pesada: se recore todo el vector
+        for(int i=0; i<tamGrid; i++)
+                if(matrizOriginal[i]!=matrizJugador[i]) resultado=false;               
+        */
+        
+               
+        //De esta forma si el error está al principio no tiene que recorrer toda la matriz
+        int pos=0;
+        while(resultado==true && pos<tamGrid){
+            if(matrizOriginal[pos]!=matrizJugador[pos]) resultado=false;
+            pos++;
+        }
+            
+        
+        return resultado;
+    }
+    
+    
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
-        boolean matriz[] = obtenerMatrizJugada(2,12);
-        leerMatriz(matriz, 3, 4);
+        int numFilas=4;
+        int numColumnas=3;
+        int numCeldas=6;
         
+        //num de celdas a obtener, numFilas, numColumnas (de la matriz)
+        boolean matriz[] = obtenerMatrizJugada(numCeldas,numFilas,numColumnas);
+       // leerMatriz(matriz, numFilas, numColumnas);
+        
+        boolean matrizJugadorA[] = obtenerMatrizJugada(numCeldas,numFilas,numColumnas);
+        boolean matrizJugadorB[] = obtenerMatrizJugada(numCeldas,numFilas,numColumnas);
+        
+        leerMatriz(matrizJugadorA, numFilas, numColumnas);
+        leerMatriz(matrizJugadorB, numFilas, numColumnas);
+        
+        if(compruebaMatrices(matrizJugadorA, matrizJugadorB, numFilas, numColumnas))
+            System.out.println("Matrices iguales");
+        else
+            System.out.println("Matrices distintas");
         
     }
     
--- app/src/main/java/butterflydevs/brainstudio/JuegoGrid12.java
+++ app/src/main/java/butterflydevs/brainstudio/JuegoGrid12.java
@@ -1,18 +1,43 @@
 package butterflydevs.brainstudio;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
+
+/**
+ * ### Explicación de la rutina: ###
+ *
+ * En esta actividad se crean 20 pantallas de juego. Se empieza preguntando por 2 celdas y este número va aumentando
+ * hasta 6 ( el grid es de doce y es el punto donde se llega el máximo de dificultad), entonces se recorren 5 niveles.
+ * Dentro de cada nivel se pregunta cuatro veces la matriz (con distinta disposición de los elementos).
+ *
+ * Cada vez que el jugador pusa cualquier boton:
+ *      1. Este se ilumina
+ *      2. Se comprueba la matriz con la original
+ *
+ *      (En este punto se pueden ahorrar bastantes comprobaciones si no se comprueba hasta haber pulsado minimo
+ *      tantos botones como se espera para la solucion. Si se esperan 5 celdas del jugador no tiene sentido ir
+ *      comprobando cuando pulsa 1, 2, 3, porque siempre dara resultado negativo.)
+ *
+ */
+
+
 
 public class JuegoGrid12 extends ActionBarActivity {
 
     //Tamaño del grid
     final private int tamGrid=12;
+    int contador =0;
 
     //Vector de botones
     private Button[] botones = new Button[tamGrid];
@@ -35,6 +60,25 @@ public class JuegoGrid12 extends ActionBarActivity {
        // animarGrid();
 
 
+     //   for(contador=0; contador<tamGrid; contador++) {
+
+            botones[0].setOnClickListener(
+
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            //Acciones del botón:
+
+                            //CAmbiarlo de color
+                            botones[0].setBackgroundColor(Color.RED);
+
+                            //Setearle un color y aplicarle una iluminación rápida.
+                            botones[0].setBackgroundColor(Color.RED);
+
+                        }
+                    }
+            );
+       // }
 
 
     }
@@ -43,14 +87,18 @@ public class JuegoGrid12 extends ActionBarActivity {
     protected void onStart(){
         super.onStart();
         animarGrid();
-        for(int i=0; i<10; i++) {
-            obtenerMatrizJugada(2);
-            System.out.println("---");
-        }
 
-        matrizJugada=obtenerMatrizJugada(2);
+        //Obtenemos la matriz de la jugada que el jugador debe resolver
+        matrizJugada=obtenerMatrizJugada(2,4,3);
+
+
+
+
+
+
 
     }
+
 
     public void asociarElementosVista(){
         String buttonID;
@@ -64,6 +112,10 @@ public class JuegoGrid12 extends ActionBarActivity {
         }
 
     }
+
+    /**
+     * Función para animar el grid al entrar en la actívity
+     */
     public void animarGrid(){
         //Cargamos la animación del botón:
         botones[0].startAnimation(loopParpadeante);
@@ -72,16 +124,47 @@ public class JuegoGrid12 extends ActionBarActivity {
           //  botones[i].startAnimation(loopParpadeante);
     }
 
+    /**
+     * Función para comparar la matriz original y la que crea el usuario
+     * @param matrizOriginal La matriz de referencia (la correcta)
+     * @param matrizJugador  La matriz que crea el jugador al pulsar los botones
+     * @param numFilas Número de filas de las matrices
+     * @param numColumnas Número de columnas de las matrices
+     * @return true si las matrices son iguales y false si no lo son
+     */
+    public boolean compruebaMatrices(boolean matrizOriginal[], boolean matrizJugador[], int numFilas, int numColumnas){
+
+        /*
+        El único objetivo es compara si dos matrices son iguales, para eso comprobaremos celda a celda.
+        */
+
+        boolean resultado=true;
+        int tamGrid=numFilas*numColumnas;
+
+        //De esta forma si el error está al principio no tiene que recorrer toda la matriz
+        int pos=0;
+        while(resultado==true && pos<tamGrid){
+            if(matrizOriginal[pos]!=matrizJugador[pos]) resultado=false;
+            pos++;
+        }
+
+        return resultado;
+    }
 
     /**
-     * Esta función es la que define que celdas serán las que se marquen en el juego. Si se le pasa
-     * dos tendrá que calcular dos aleatoriamente y asignarlas en una matriz de booleanos.
-     * @param numCeldas Es el número de celdas que tenra que calcular de forma aleatoria.
+     * Esta es la función que se copiará en el código de AndroidStudio
+     * @param numCeldas El número de celdas a con las que jugar.
+     * @param numFilas El número de filas que tiene el grid de la jugada.
+     * @param numColumnas El número de columnas que tiene el grid de la jugada.
+     * @return
      */
-    public boolean[] obtenerMatrizJugada(int numCeldas){
+    public boolean[] obtenerMatrizJugada(int numCeldas, int numFilas, int numColumnas){
 
+        boolean depuracion=false;
+
+        int tamGrid=numFilas*numColumnas;
         //Creamos el vector que vamos a devolver:
-        boolean [] matrizBooleanos = new boolean[12];
+        boolean [] matrizBooleanos = new boolean[tamGrid];
 
         //Inicializamos el vector:
         for(int i=0; i<tamGrid; i++)
@@ -90,31 +173,57 @@ public class JuegoGrid12 extends ActionBarActivity {
         //Rellenamos la matriz con el número de celdas positivas que nos indique el parámetro:
 
 
-            //Random que hace imposible repetir dos veces un número:
-                ArrayList enteros = new ArrayList();
-                for(int i=0; i<tamGrid; i++)
-                    enteros.add(i);
+        //Random que hace imposible repetir dos veces un número:
+        List<Integer> enteros = new ArrayList<>();
+        for(int i=0; i<tamGrid; i++)
+            enteros.add(i);
+
+        if(depuracion){
+            for(int elemento: enteros)
+                System.out.print(elemento);
+            System.out.println("");
+        }
+
 
         ArrayList finales = new ArrayList();
 
-            //Sacamos cuantos numeros nos sean necesarios como celdas tengamos que rellenar.
-            int elegido;
-            int numFinal=tamGrid;
-            Random rnd = new Random();
-            for(int i=0; i<numCeldas; i++) {
-                elegido = (int)(rnd.nextDouble() * numFinal); //Random de números entre el 0 y el numFinal
-                enteros.remove(elegido);
-                finales.add(elegido);
-                numFinal--;
+        //Sacamos cuantos numeros nos sean necesarios como celdas tengamos que rellenar.
+        int elegido;
+        int numero;
+        int numFinal=tamGrid;
+        Random rnd = new Random();
+        for(int i=0; i<numCeldas; i++) {
+            elegido = (int)(rnd.nextDouble() * numFinal); //Random de números entre el 0 y el numFinal
+            if(depuracion) System.out.println("elegido: "+elegido);
+
+            if(depuracion){
+                System.out.println("Vector de enteros antes");
+                for(int elemento: enteros)
+                    System.out.print(elemento);
+                System.out.println("");
+
+
+                System.out.println("Vector de enteros después");
+                for(int elemento: enteros)
+                    System.out.print(elemento);
+                System.out.println("");
             }
 
-            for(int i=0; i<finales.size(); i++)
-                System.out.println("Elegido: "+finales.get(i));
+            finales.add(enteros.get(elegido));
+            enteros.remove(elegido);
+            numFinal--;
+        }
+
+        for(int i=0; i<finales.size(); i++){
+            int elemento = (int)finales.get(i);
+            matrizBooleanos[elemento]=true;
+        }
 
 
 
 
         return matrizBooleanos;
     }
+
 
 }
