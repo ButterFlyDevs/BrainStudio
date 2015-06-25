6ee0fbe3f3cb96a6d557b53578b4c7b4001a52cc
JA-Gonz <jagonccc@gmail.com>
2015-06-25 12:11:15 +0200

    Push pa ver si revienta después de tocar el gradle y volver ale stado anterior

--- app/src/main/java/butterflydevs/brainstudio/Help.java
+++ app/src/main/java/butterflydevs/brainstudio/Help.java
@@ -1,17 +1,47 @@
 package butterflydevs.brainstudio;
 
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
+/**
+ * Activity para mostrar ayuda al usuario de la aplicación.
+ *
+ * La Activity reconocerá por los datos pasados a través del Intent desde qué Activity se le está llamando
+ * y cargará los recursos necesarios para ella.
+ */
 public class Help extends ActionBarActivity {
+
+    private String zona_de_llamada; //Podrá ser desde el activity de resultados de un juego concreto, o desde el tablero de jugo
+    private int numero_zona;    //Del 1 al 5 (POR AHORA! :D ), define el juego que está invocando a esta clase
+
+    private TextView titulo, descripcion_peque, descripcion_larga;
+    private ImageView imagen_ayuda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        //Con esta orden conseguimos hacer que no se muestre la ActionBar.
+        getSupportActionBar().hide();
+        //Con esta hacemos que la barra de estado del telefono no se vea y la actividad sea a pantalla completa.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        titulo = (TextView) findViewById(R.id.titulo_ayuda);
+        descripcion_larga = (TextView) findViewById(R.id.descripcion_larga_ayuda);
+        descripcion_peque = (TextView) findViewById(R.id.descripcion_peque_ayuda);
+        imagen_ayuda = (ImageView) findViewById(R.id.imagen_ayuda);
+
+
     }
 
     @Override
